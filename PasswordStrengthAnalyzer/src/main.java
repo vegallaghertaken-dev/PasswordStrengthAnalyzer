64ebe802cf934a9d4cd24b5f20d222a83d372f19
vinny <vegallaghertaken@gmail.com>
2025-10-01 12:17:39 -0400

    Updated password rules and suggestions

--- PasswordStrengthAnalyzer/src/main.java
+++ PasswordStrengthAnalyzer/src/main.java
@@ -1,5 +1,12 @@
+/* Developer Notes 
+ *
+ * 'main.java' serves as a file to handle user input/output
+ * ex: request password, call analyzer, visualize result
+ * 
+ */
 package PasswordStrengthAnalyzer.src;
 
-public class main {
-    
+class main {
+    int x = 1;
 }
+
